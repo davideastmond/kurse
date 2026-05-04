1c05587f68b735f7993e1f9fbb44a2faa1f47174
David Eastmond <davebuck1981@gmail.com>
2026-05-04 12:11:47 -0400

    feat: add experimental serverActions and configure remote image patterns in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "2mb",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-4693e6acc45b4e9c94db009547fec7a8.r2.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
