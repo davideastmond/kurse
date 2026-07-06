7e049340a187f270c601fee890c44e2a9c0ec64d
David Eastmond <davebuck1981@gmail.com>
2026-07-06 14:29:20 -0400

    test coverage command and update config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 
 const configDir = path.dirname(fileURLToPath(import.meta.url));
 
@@ -14,5 +14,8 @@ export default defineConfig({
   },
   test: {
     environment: "node",
+    coverage: {
+      exclude: [...(configDefaults.coverage.exclude || []), "src/db/schema.ts"],
+    },
   },
 });
